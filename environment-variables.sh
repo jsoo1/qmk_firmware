c1e40458964291b33c2212d7e70c179ed872899b
John Soo <jsoo1@asu.edu>
2018-08-22 08:49:46 -0700

    Move environment variable setup to script file.

---
+++ environment-variables.sh
@@ -0,0 +1,7 @@
+#! $GUIX_ENVIRONMENT/bin/env bash
+
+avr_flags="-isystem $GUIX_ENVIRONMENT/avr/include -B $GUIX_ENVIRONMENT/avr/lib/avr5 -L $GUIX_ENVIRONMENT/avr/lib/avr5 -B $GUIX_ENVIRONMENT/avr/lib/avr35 -L $GUIX_ENVIRONMENT/avr/lib/avr35 -B $GUIX_ENVIRONMENT/avr/lib/avr51 -L $GUIX_ENVIRONMENT/avr/lib/avr51"
+
+export CFLAGS="$avr_flags"
+export ASFLAGS="$avr_flags"
+export C_INCLUDE_PATH=""
--- manifest.scm
+++ manifest.scm
@@ -1,31 +1,19 @@
 (use-modules (gnu packages avr)
+             (gnu packages base)
              (gnu packages compression)
              (gnu packages embedded)
              (gnu packages flashing-tools)
              (gnu packages gcc)
-             (gnu packages wget))
+             (gnu packages wget)
+             (guix packages))
 
-(define avr-flags
-  (apply string-append
-         (map (lambda (flag) (string-append (car flag) " " "/gnu/store/nprgsdpm31n8n7wdrvbx358prc83n5bi-profile" (cadr flag) " "))
-              `(("-isystem" "/avr/include")
-                ("-B"       "/avr/lib/avr5")
-                ("-L"       "/avr/lib/avr5")
-                ("-B"       "/avr/lib/avr35")
-                ("-L"       "/avr/lib/avr35")
-                ("-B"       "/avr/lib/avr51")
-                ("-L"       "/avr/lib/avr51")))))
 
-(setenv "CFLAGS" avr-flags)
-(setenv "ASFLAGS" avr-flags)
-(unsetenv "C_INCLUDE_PATH")
-
-(packages->manifest (list dfu-util
-                          dfu-programmer
-                          gnu-make
-                          unzip
-                          wget
-                          zip
-                          gcc
-                          avr-toolchain-5
-                          arm-none-eabi-toolchain-6) )
+`(,dfu-util
+  ,dfu-programmer
+  ,gnu-make
+  ,unzip
+  ,wget
+  ,zip
+  ,gcc
+  ,arm-none-eabi-toolchain-6
+  ,avr-toolchain-5)
