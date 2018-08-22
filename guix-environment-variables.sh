5f73a43425b387c7b67cceb7e4e1923f86268f7f
John Soo <jsoo1@asu.edu>
2018-08-22 08:53:42 -0700

    Put guix files under common name.

--- guix-environment-variables.sh
+++ guix-environment-variables.sh
@@ -1,4 +1,4 @@
-#! $GUIX_ENVIRONMENT/bin/env bash
+#! $GUIX_ENVIRONMENT/bin/env sh
 
 avr_flags="-isystem $GUIX_ENVIRONMENT/avr/include -B $GUIX_ENVIRONMENT/avr/lib/avr5 -L $GUIX_ENVIRONMENT/avr/lib/avr5 -B $GUIX_ENVIRONMENT/avr/lib/avr35 -L $GUIX_ENVIRONMENT/avr/lib/avr35 -B $GUIX_ENVIRONMENT/avr/lib/avr51 -L $GUIX_ENVIRONMENT/avr/lib/avr51"
 
